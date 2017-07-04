0daa8c02739c860b8b7cf04d534b1b9f6a763ea0
alan-lira <alan_lira@globomail.com>
2017-07-04 01:07:54 -0300

    Update main.py
    Coisas para resolver neste código:
    1) Remover problema de ambiguidade entre o '=' no sentido de assign e o '=' no sentido de compare. Para fazer funcionar o código de exemplo do professor, tive que alterar todos os assigns para '}'.
    2) Definir uma regra t_error.

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
-import re
 import ply.lex as lex
 import ply.yacc as yacc
- 
+
 ## Lexer part
-tokens = (
-    'INTEGER',
-    'ID',
-    'OP_REL',
-    'OP_ARIT',
-    'COMMENT',
-    'EQ',
-    'SEMI',
-    'COL',
-    'LPAREN',
-    'RPAREN',
-    'IF',
-    'THEN',
-    'ELSE',
-    'DEF'
-)
+
+tokens = []
+
+keywordlist = ['if', 'then', 'else', 'def']
+
+RESERVED = {}
+for keyword in keywordlist:
+	name = keyword.upper()
+	RESERVED[keyword] = name
+	tokens.append(name)
+
+tokens = tuple(tokens) +\
+         (
+         'INTEGER',
+         'ID',
+         'OP_REL',
+         'OP_ARIT',
+         'COMMENT',
+         'EQ',
+         'SEMI',
+         'COL',
+         'LPAREN',
+         'RPAREN'
+         )
 
 def t_INTEGER(t):
     r'[0-9][0-9]*'
     t.value = int(t.value)
     return t
 
-t_IF = r'if'
-t_THEN = r'then'
-t_ELSE = r'else'
-t_DEF = r'def'
-t_ID = r'[a-zA-Z]([0-9]|[a-zA-Z]|_)*'
-t_EQ = r'='
+def t_ID(t):
+   r'[a-zA-Z]([0-9]|[a-zA-Z]|_)*'
+   t.type = RESERVED.get(t.value, "ID")
+   return t
+
+t_EQ = r'}'
 t_OP_REL = r'[>|<|=]'
 t_OP_ARIT = r'[+\-*/]'
 t_COMMENT = r'//[^\n]*|/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
@@ -38,24 +45,20 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_COL = r','
 t_SEMI = r';'
+t_IF = r'if'
+t_THEN = r'then'
+t_ELSE = r'else'
+t_DEF = r'def'
 
 t_ignore = '[ \n\t\r\f]'
 
 lexer = lex.lex(debug=1)
-s = input()
-lexer.input(s)
-while True:
-    token = lexer.token()
-    if not token:
-        break
-    print(token)
+
 ## Parser part
-start = 'start'
- 
+
 def p_P(p):
     ''' P : D F
     '''
-    p[0] = (p[1], p[2])
 
 def p_D(p):
     ''' D : ID EQ INTEGER SEMI D
@@ -86,8 +89,16 @@ def p_E(p):
     '''
 
 start = 'P'
- 
+
 parser = yacc.yacc(debug=1)
- 
+
 ## Test it
+s = '''a } 312;
+       b } 111;
+       def mdc(a,b) }
+          if mod(a,b) = 0 then b
+          else mdc(b,mod(a,b));
+       def mod(a,b) }
+          if a < b then a
+          else mod(a-b,b);'''
 print(parser.parse(s))
