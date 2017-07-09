6ee32429194d48995d345cb808dcdc3b7041e6be
alan-lira <alan_lira@globomail.com>
2017-07-09 18:50:59 -0300

    Update cgen.py

--- cgen.py
+++ cgen.py
@@ -15,3 +15,15 @@ class CGen:
                 'lw $t1 4($sp)' +\
                 'add $a0 $t1 $a0' +\
                 'addiu $sp $sp 4'
+
+    def f_call(self, p):
+        string_builder = ""
+        for i in p[::-1]:
+            arg = str(self.integer(p[i]))
+            string_builder+='\n'+arg
+            string_builder+='\nsw $a0 0($sp)'
+            string_builder+= '\naddiu $sp $sp -4'
+        return 'sw $fp 0($sp)' +\
+               '\naddiu $sp $sp -4' +\
+               string_builder +\
+               '\njal f_entry'
