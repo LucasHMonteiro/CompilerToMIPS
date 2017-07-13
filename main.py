0f339058dba186ea8619acd6ad55890038949501
alan-lira <alan_lira@globomail.com>
2017-07-13 17:37:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from cgen import CGen
 
 cgen = CGen()
 
+
 def cat_list(init_list, list_end):
     if type(list_end) == type([]):
         return init_list + list_end
     else:
         init_list.append(list_end)
+
 
 func_hash = {
     '>': lambda x, y: x > y,
@@ -21,6 +23,7 @@ func_hash = {
     '/': lambda x, y: x // y
 }
 
+tree = []
 symbol_table = {}
 first_pass = True
 var_list = []
@@ -33,39 +36,42 @@ keywordlist = ['if', 'then', 'else', 'def']
 
 RESERVED = {}
 for keyword in keywordlist:
-	name = keyword.upper()
-	RESERVED[keyword] = name
-	tokens.append(name)
+    name = keyword.upper()
+    RESERVED[keyword] = name
+    tokens.append(name)
 
-tokens = tuple(tokens) +\
+tokens = tuple(tokens) + \
          (
-         'INTEGER',
-         'ID',
-         'OP_REL',
-         'OP_ADD',
-         'OP_MULT',
-         'COMMENT',
-         'EQ',
-         'SEMI',
-         'COL',
-         'LPAREN',
-         'RPAREN'
+             'INTEGER',
+             'ID',
+             'OP_REL',
+             'OP_ADD',
+             'OP_MULT',
+             'COMMENT',
+             'EQ',
+             'SEMI',
+             'COL',
+             'LPAREN',
+             'RPAREN'
          )
+
 
 def t_INTEGER(t):
     r'[0-9][0-9]*'
     t.value = int(t.value)
     return t
 
+
 def t_ID(t):
-   r'[a-zA-Z]([0-9]|[a-zA-Z]|_)*'
-   t.type = RESERVED.get(t.value, "ID")
-   return t
+    r'[a-zA-Z]([0-9]|[a-zA-Z]|_)*'
+    t.type = RESERVED.get(t.value, "ID")
+    return t
+
 
 t_EQ = r'='
 t_OP_REL = r'[>|<]|=='
-t_OP_ADD = r'[\+\-]'
-t_OP_MULT = r'[\*\/]'
+t_OP_ADD = r'[\+|\-]'
+t_OP_MULT = r'[\*|\/]'
 t_COMMENT = r'//[^\n]*|/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
@@ -78,11 +84,14 @@ t_DEF = r'def'
 
 t_ignore = '[ \n\t\r\f]'
 
+
 def t_error(t):
-    print("Illegal character '%s' at line' %s'" % (t.value[0] , t.lexer.lineno ))
+    print("Illegal character '%s' at line' %s'" % (t.value[0], t.lexer.lineno))
     t.lexer.skip(1)
 
+
 lexer = lex.lex(debug=1)
+
 
 ## Parser part
 
@@ -96,16 +105,19 @@ def p_P(p):
             var_list.append(p[1])
         if not set(var_list) == set(args_list) and p.slice[1].lexpos == 1:
             raise Exception('Arguments in the first function not declared')
+    if(len(p) > 2):
+      if first_pass:
+         p[0] =  ('CGEN_ASSIGN',p[1], p[3])
+         tree.append(p[0])
 
 def p_I(p):
     ''' I : D I
           | D
-    '''    
+    '''
 
 def p_D(p):
     ''' D : DEF ID LPAREN ARGS RPAREN EQ E SEMI
     '''
-
     if first_pass:
         if p[2] in symbol_table:
             raise Exception('Function name already used')
@@ -115,19 +127,21 @@ def p_D(p):
         if not set(p[4]) == set(var_list):
             msg = ''
             for id in set(var_list) - set(p[4]):
-                msg += 'Variable '+id+' not declared\n'
+                msg += 'Variable ' + id + ' not declared\n'
             raise Exception(msg)
-        var_list = []
+        p[0] = ('CGEN_DEF', p[2], len(var_list), p[7])
+        tree.append(p[0])
+        #var_list = []
 
-    
-    
+
+
 def p_ARGS(p):
     ''' ARGS : ID COL ARGS
              | ID
     '''
     global args_hash
     if len(p) > 2:
-        first = {p[1]: len(p[3])+1}
+        first = {p[1]: len(p[3]) + 1}
         first.update(args_hash)
         args_hash.update(first)
         p[0] = [p[1]]
@@ -142,6 +156,7 @@ def p_ARGS(p):
         args_hash = {p[1]: 1}
         p[0] = p[1]
 
+
 def p_SEQ(p):
     ''' SEQ : E COL SEQ
             | E
@@ -152,6 +167,7 @@ def p_SEQ(p):
     else:
         p[0] = p[1]
 
+
 def p_E(p):
     ''' E : INTEGER
           | ID
@@ -159,50 +175,68 @@ def p_E(p):
           | ARIT_EXP
           | ID LPAREN SEQ RPAREN
     '''
-    
     if p.slice[1].type == 'ID' and len(p) == 2 and not first_pass:
-       global var_list
-       if not p[1] in var_list:
-           var_list.append(p[1])
+        global var_list
+        if not p[1] in var_list:
+            var_list.append(p[1])
+        p[0] = p[1]
 
-    if len(p) == 5 and not first_pass :
+    if p.slice[1].type == 'INTEGER':
+        p[0] = p[1]
+
+    if p.slice[1].type == 'ARIT_EXP':
+        p[0] = p[1]
+
+    if len(p) == 5 and not first_pass:
         if p[1] not in symbol_table:
             raise Exception('Function not declared')
         else:
             if not len(p[3]) == len(symbol_table[p[1]]):
                 raise Exception("Wrong number of parameters expected: %d, given: %s" % (len(symbol_table[p[1]]), p[3]))
+        p[0] = ('CGEN_CALL', p[1], p[3])
 
-    global args_hash
-    if not p.slice[1].type == 'ARIT_EXP':
-        print(cgen.e(p, args_hash))
-    args_hash = {}
+    if len(p) == 9:
+        p[0] = ('CGEN_IF',p[2],p[3],p[4],p[6],p[8])
 
 def p_ARIT_EXP(p):
     ''' ARIT_EXP : ARIT_EXP OP_ADD TERM
                  | TERM
     '''
     if len(p) > 2:
-        cgen.arit(p)
+        if(p[2] == '+'):
+            p[0] = ('CGEN_SUM', p[1], p[3])
+        else:
+            p[0] = ('CGEN_SUB', p[1], p[3])
+    else:
+        p[0] = p[1]
 
 def p_TERM(p):
     ''' TERM : TERM OP_MULT FACTOR
              | FACTOR
     '''
     if len(p) > 2:
-        cgen.term(p)
+        if(p[2] == '*'):
+            p[0] = ('CGEN_MULT', p[1], p[3])
+        else:
+            p[0] = ('CGEN_DIV', p[1], p[3])
+    else:
+        p[0] = p[1]
 
 def p_FACTOR(p):
     ''' FACTOR : PAREN
                | E
     '''
+    p[0] = p[1]
 
 def p_PAREN(p):
     ''' PAREN : LPAREN E RPAREN
     '''
-    cgen.paren(p)
+    p[0] = p[2]
+
 
 def p_error(p):
     print("Syntax error at token '%s' of type '%s' at position '%s'" % (p.value, p.type, p.lexpos))
+
 
 start = 'P'
 
@@ -212,15 +246,10 @@ parser = yacc.yacc(debug=1)
 s = ''' a = 1;
         b = 2;
         def sum(a,b) =
-            a + b;
+            a;
+        def sub(a,b) = sum(if a == 2 then 4 else 5,b);
     '''
-# lexer.input(s)
-# while True:
-#     token = lexer.token()
-#     if not token:
-#         break
-#     print(token)
-
 parser.parse(s)
 first_pass = False
-print(parser.parse(s))
+parser.parse(s)
+print(tree)
